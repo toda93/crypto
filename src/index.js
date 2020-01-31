138d0628055c9607157bde97c963122f4f5ad0e3
toda93 <dttong93@gmail.com>
2020-01-31 16:33:09 +0700

    sync

--- src/index.js
+++ src/index.js
@@ -1,44 +1,43 @@
 import crypto from 'crypto';
 import url from 'url';
 
-export function md5(value) {
-    return crypto.createHash('md5').update(value.toString()).digest('hex');
+export function md5(text) {
+    return crypto.createHash('md5').update(text).digest('hex');
 }
-export function sha1(value) {
-    return crypto.createHash('sha1').update(value.toString()).digest('hex');
+export function sha1(text) {
+    return crypto.createHash('sha1').update(text).digest('hex');
+}
+export function sha1Secret(text, key) {
+    return crypto.createHmac('sha1', key).update(text).digest('hex');
 }
 
-export function encyptAES(key, value) {
+
+export function encyptAES(text, key) {
     let cipher = crypto.createCipher('aes-256-cbc', key);
-    let crypted = cipher.update(value.toString(), 'utf8', 'hex');
+    let crypted = cipher.update(text, 'utf8', 'hex');
     crypted += cipher.final('hex');
     return crypted;
 }
 
 export function decryptAES(key, hash) {
     let decipher = crypto.createDecipher('aes-256-cbc', key);
-    let dec = decipher.update(hash.toString(), 'hex', 'utf8');
+    let dec = decipher.update(hash, 'hex', 'utf8');
     dec += decipher.final('utf8');
     return dec;
 }
-function removeWebSafe(safeEncodedString) {
-    return safeEncodedString.replace(/-/g, '+').replace(/_/g, '/');
-}
 
-function makeWebSafe(encodedString) {
-    return encodedString.replace(/\+/g, '-').replace(/\//g, '_');
-}
 
-function decodeBase64Hash(code) {
-    return Buffer.from ? Buffer.from(code, 'base64') : new Buffer(code, 'base64');
-}
+export function toSign(path, secret) {
 
-function encodeBase64Hash(key, data) {
-    return crypto.createHmac('sha1', key).update(data).digest('base64');
-}
-export function sign(path, secret) {
+    if (path.includes('?')) {
+        path += '&time=';
+    } else {
+        path += '?time=';
+    }
+
+    path += Math.floor(Date.now() / 1000);
+
     const uri = url.parse(path);
-    const safeSecret = decodeBase64Hash(removeWebSafe(secret));
-    const hashedSignature = makeWebSafe(encodeBase64Hash(safeSecret, uri.path));
+    const hashedSignature = sha1Secret(secret, uri.path);
     return url.format(uri) + '&sign=' + hashedSignature;
 }
